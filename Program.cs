f6235b9bf2cd34c17c178161545021e50117f3e8
Randeep2004 <149605904+Randeep2004@users.noreply.github.com>
2024-04-20 23:12:23 -0600

    Randeep

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ConnectFour
+{
+    private const int ROWS = 6;
+    private const int COLUMNS = 7;
+    private int[,] board;
+    private int currentPlayer;
+
+
+
+
+
+}
