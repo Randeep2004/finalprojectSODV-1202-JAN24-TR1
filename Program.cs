14241284f194e967a82bc9b6a21215db6a1f99c0
Randeep2004 <149605904+Randeep2004@users.noreply.github.com>
2024-04-24 16:09:49 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿   using System;
 
 public class ConnectFour
 {
@@ -51,5 +51,61 @@ public class ConnectFour
             }
         }
         return false; // Column is full
+    }
+    public bool CheckWin()
+    {
+        // Check horizontally
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLUMNS - 3; j++)
+            {
+                if (board[i, j] == currentPlayer && board[i, j + 1] == currentPlayer &&
+                    board[i, j + 2] == currentPlayer && board[i, j + 3] == currentPlayer)
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Check vertically
+        for (int i = 0; i < ROWS - 3; i++)
+        {
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                if (board[i, j] == currentPlayer && board[i + 1, j] == currentPlayer &&
+                    board[i + 2, j] == currentPlayer && board[i + 3, j] == currentPlayer)
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Check diagonally (top-left to bottom-right)
+        for (int i = 0; i < ROWS - 3; i++)
+        {
+            for (int j = 0; j < COLUMNS - 3; j++)
+            {
+                if (board[i, j] == currentPlayer && board[i + 1, j + 1] == currentPlayer &&
+                    board[i + 2, j + 2] == currentPlayer && board[i + 3, j + 3] == currentPlayer)
+                {
+                    return true;
+                }
+            }
+        }
+
+        // Check diagonally (top-right to bottom-left)
+        for (int i = 0; i < ROWS - 3; i++)
+        {
+            for (int j = 3; j < COLUMNS; j++)
+            {
+                if (board[i, j] == currentPlayer && board[i + 1, j - 1] == currentPlayer &&
+                    board[i + 2, j - 2] == currentPlayer && board[i + 3, j - 3] == currentPlayer)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
